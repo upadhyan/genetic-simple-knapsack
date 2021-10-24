963425052ee6e27e45898cbfb93ce986f34625ee
upadhyan <49886694+upadhyan@users.noreply.github.com>
2021-10-24 10:40:32 -0400

    fitness function made

--- knapsack_algo.R
+++ knapsack_algo.R
@@ -1,3 +1,10 @@
 source('knapsack_configuration.R')
 config_ga_knapsack() # add rstudio = FALSE if not on rstudio
 
+fitness <- function(solution, profit){
+ return(solution %*% profit)
+}
+
+profits = c(1,2,3,5,5)
+solution = c(1,0,1,0, 1)
+print(fitness(solution, profits))
