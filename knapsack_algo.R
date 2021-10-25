05160dfec7f999ccb31b2917bdebf89838fcb37c
upadhyan <49886694+upadhyan@users.noreply.github.com>
2021-10-25 17:05:27 -0400

    Created New Fitness Function

--- knapsack_algo.R
+++ knapsack_algo.R
@@ -3,7 +3,7 @@ config_ga_knapsack() # add rstudio = FALSE if not on rstudio
 
 
 
-fitness <- function(solution, profit, weights, c){
+fitness_zero <- function(solution, profit, weights, c){
   weight <- weights %*% solution
   if(weight > c){
     return(0)
@@ -13,12 +13,18 @@ fitness <- function(solution, profit, weights, c){
   }
 }
 
-gabin_raMutation <- function(object, parent, ...)
-{
-  if(gaControl("useRcpp"))
-    gabin_raMutation_Rcpp(object, parent)
-  else
-    gabin_raMutation_R(object, parent)
+fitness_constraint_adjust <- function(solution, profit, weights, c){
+  weight <- weights %*% solution
+  max_profit = sum(profit)
+  max_weight = sum(weights)
+  if (weight > c){
+    g = 0
+    h = 1 - (weight - c)/(max_weight - c)
+  } else{
+    g <- profit %*% solution
+    h = 1
+  }
+  return(h*max_profit + g)
 }
 
 sp_mutation <- function(object, parent,weights, profit)
@@ -57,19 +63,24 @@ ratio_prob_mutation <- function(object, parent,weights, profit)
   return(mutate)
 }
 
-weights = c(2,2,2,2,2,2,2,9)
-profit = c(3,2,3,2,3,2,2,50)
-c = 8
-fitness(solution, profit, weights, c)
+profit = c(32,22,3,67,6,10,22,54,67,55)
+weights = c(42,24,1,57,7,11,12,64,77,35)
+c = 77+35+1+24+3
 
-GA <- ga(type = "binary",
-         fitness = function(x) fitness(x,profit, weights, c), 
-         mutation = function(object, parent, ...) sp_mutation(object, parent, weights, profit),
-         nBits=8,
-         popSize = 100,
-         maxiter = 1000,
-         pcrossover = 0.8, 
-         pmutation = 0.1, 
-         elitism = base::max(1, round(popSize*0.05)),
-         run = 100)
-summary(GA)
+
+GA1 <- ga(type = "binary", fitness = function(x) fitness_constraint_adjust(x, profit, weights, c),
+          mutation = function(object, parents, ...) sp_mutation(object, parents, weights, profit),
+          nBits = 10, 
+          popSize = 100, maxiter = 1000, run = 200)
+
+
+summary(GA1)
+
+solution = c(0,1,1,1,0,1,1,0,0,1)
+if(solution %*% weights <= c){
+  print("Good")
+  print(c)
+  print(solution %*% weights)
+} else {
+  print("bad")
+}
